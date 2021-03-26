1940e0cd5ecf5053c8502323057a06818ce3e1c0
dato <qutaiseli15@gmail.com>
2021-03-26 08:35:08 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const app = require('express')()
+const http = require ('http').Server(app)
+const io = require ('socket.io')(http)
+const port = 8080;
+const path = require('path')
+
+// app.get('/', (req, res)=>{
+//     res.sendFile(`${__dirname}/index.html`)
+// })
+console.log(__dirname)
+app.use(require('express').static(path.join(__dirname, "../chat-1")))
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message',  msg);
+    });
+});
+
+http.listen(port, ()=>{
+    console.log(`app listening ${port}`)
+})
